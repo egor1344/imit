a6aa390a3e5e2f112ae4a3492e21548366541a72
Egor <gjyzdby@yandex.ru>
2017-04-25 00:33:44 +0500

    Закончил распологать блоки

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync');
+
+gulp.task('sass', function(){ // Создаем таск Sass
+    return gulp.src('app/sass/**/*.sass') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+        .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
+
+gulp.task('browser-sync', function () {
+  browserSync({
+    server:{
+      baseDir: 'app'
+    },
+    nofify:false
+  });
+});
+
+
+gulp.task('watch', ['browser-sync', 'sass'], function() {
+  gulp.watch('app/sass/**/*.sass', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
+});
