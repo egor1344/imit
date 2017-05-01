a7ab32c4faa520de4089feefbd69744f9dba68a5
Egor <gjyzdby@yandex.ru>
2017-05-01 22:20:09 +0500

    Третий график готов

--- app/js/chart_three.js
+++ app/js/chart_three.js
@@ -11,18 +11,35 @@ $(document).ready(function(){
 
 
     $('#th_charts').on('click', 'button', function () {
-      console.log('Вжух');
+      // console.log('Вжух');
       var a = Number($('#num31').val());
-      console.log(a);
+      // console.log(a);
       var b = Number($('#num32').val());
-      console.log(b);
       var dat = [
-        ['t', 'f(t)'],
+        ['t', 'Вероятность отказа'],
       ];
-      for (var i = a; i<b; i++) {
-        dat.push([i,normalRasp(i, 14, 2)]);
+      var rez = 0.0;
+      var rand = 0.0;
+      var fail = 0;
+      var total_fail = 0;
+      for (var i = a; i <= b; i++) {
+        rez = normalRasp(i, 14, 2);
+        fail = 0;
+        for (var j = 0; j < 20000; j++) {
+          rand = Math.random()
+          if (rez >= rand) {
+            fail = fail + 1;
+            total_fail = total_fail + 1;
+          }
+        }
+      console.log(fail, total_fail);
+      fail = 1 - (fail / 20000);
+      dat.push([i, fail]);
       }
-      drawChart(dat, 'Третий компонент системы', 'three_charts')
+      console.log(b-a);
+      total_fail = 1 - (total_fail / (20000 * (b - a + 1)));
+      $('#summary_three').text(Math.fround(total_fail));
+      drawChart(dat, 'Графие отказов для третего компонента системы', 'three_charts');
     })
 
 });
